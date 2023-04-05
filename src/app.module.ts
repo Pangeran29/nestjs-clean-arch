cb046bcf8dc4654ba99bf08693ddf7e67f855185
Pangeran29 <pangeranjonathan1@gmail.com>
2023-04-05 11:24:05 +0700

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './modules/user/user.module';
-import { PostgresModule } from './core/database/postgres/postgres.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    UserModule,
-    PostgresModule,
-  ],
-  controllers: [],
-  providers: [],
+    ConfigModule.forRoot({ isGlobal: true })
+  ]
 })
 export class AppModule {}
